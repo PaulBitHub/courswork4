f5fe83459f9a4d5b5fe77251e6faa1f897c6ccc1
PaulBitHub <p.bitiakov.official@gmail.com>
2024-07-24 20:30:07 +0300

    Добавлена документация к классам и методам

--- src/json_saver.py
+++ src/json_saver.py
@@ -9,7 +9,7 @@ from src.vacancies import Vacancies
 class JSONSaver(Vacancies, Saver):
 
     def write_data(self):
-
+        """Метод проверки и записи данных в json файл"""
         try:
             data = json.load(open(VACANCIES_PATH))
         except FileNotFoundError:
@@ -21,6 +21,7 @@ class JSONSaver(Vacancies, Saver):
             json.dump(self.to_list_dict(), file, indent=7, ensure_ascii=False)
 
     def get_data(self):
+        """Метод по открытию и использованию данных, записанных в json файле"""
         with open(VACANCIES_PATH, encoding="utf-8") as file:
             data = json.load(file)
             self.__all_vacancies = []
@@ -28,7 +29,5 @@ class JSONSaver(Vacancies, Saver):
                 self.all_vacancies.append(Vacancy.vacancies_lst(vacancy))
 
     def del_data(self, data_json):
+        """Очистка данных из файла json с вакансиями"""
         del data_json
-
-
-
